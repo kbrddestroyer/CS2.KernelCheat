a1855b219ab6771236b85a21ddcdda2c62e834a7
Keyboard Destroyer <43577127+kbrddestroyer@users.noreply.github.com>
2024-06-23 23:48:38 +0300

    USERMODE added bhop cheat for test purposes

--- usermode/src/core/km_mailbox.cpp
+++ usermode/src/core/km_mailbox.cpp
@@ -6,7 +6,7 @@ int kmControllerEntry()
 
 	if (dPid == 0)
 	{
-		MessageBox(NULL, L"Please, open target process first!", L"Warning", MB_OK);
+		MessageBox(NULL, L"Please, open target process first! [cs2.exe]", L"Warning", MB_OK);
 		return EXIT_FAILURE;
 	}
 
@@ -21,10 +21,64 @@ int kmControllerEntry()
 	if (driver::attach(hDriverHandle, dPid) == true)
 	{
 		MessageBox(NULL, L"Succecssfully attached kernelmode to target", L"Success", MB_ICONINFORMATION | MB_OK);
+
+		initialize(hDriverHandle, dPid);
 	}
 
 	if (hDriverHandle != nullptr)
 		CloseHandle(hDriverHandle);
 
 	return EXIT_SUCCESS;
+}
+
+void initialize(HANDLE hDriver, DWORD uPid)
+{
+	if (const uintptr_t uClient = uapp::getModuleBase(uPid, L"client.dll"); uClient != 0)
+	{
+#ifdef DEBUG
+		MessageBox(NULL, L"Mapped client.dll", L"Info", MB_ICONINFORMATION | MB_OK);
+#endif
+		std::thread tBhop = std::thread(threadedBhop, std::ref(hDriver), std::ref(uClient));
+
+		tBhop.join();
+	}
+}
+
+void threadedBhop(HANDLE& hDriver, const uintptr_t& uClient)
+{
+#ifdef DEBUG
+	MessageBox(NULL, L"Mapped client.dll", L"Info", MB_ICONINFORMATION | MB_OK);
+#endif
+
+	while (true)
+	{
+#ifdef DEBUG
+		if (GetAsyncKeyState(VK_ESCAPE))
+			break;
+#endif
+		const uintptr_t playerPawn = driver::read<uintptr_t>(hDriver, uClient + offsets::client_dll::dwLocalPlayerPawn);
+
+		if (playerPawn == 0)
+			continue;
+
+		const uint32_t flags = driver::read<uint32_t>(hDriver, playerPawn + schemas::client_dll::C_BaseEntity::m_fFlags);
+		const bool bInAir = flags & (1 << 0);
+
+		const bool bSpace = GetAsyncKeyState(VK_SPACE);
+		const DWORD forceJump = driver::read<DWORD>(hDriver, uClient + buttons::jump);
+
+		if (bSpace && bInAir)
+		{
+			// Jump
+			Sleep(CFG_BHOP_DELAY);
+			driver::write(hDriver, uClient + buttons::jump, CFG_SPACE_ON);
+		}
+		else if (
+			(bSpace && !bInAir) ||
+			(!bSpace && forceJump == 65537)
+			)
+		{
+			driver::write(hDriver, uClient + buttons::jump, CFG_SPACE_OFF);
+		}
+	}
 }
